7449132ef4794890cb75a227a448546105aec023
sheemashariq1 <sheemashariq10@gmail.com>
2026-06-14 01:32:43 +0530

    Commit all

---
+++ Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/Pattern_4.cpp
@@ -0,0 +1,35 @@
+/*
+1
+22
+333
+4444
+55555
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void pattern4(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                cout << i;
+            }
+            cout << "\n";
+        }
+    }
+};
+
+// BOILERPLATE CODE
+int main()
+{
+    int n;
+    cin >> n;
+    Solution ob;
+    ob.pattern4(n);
+    return 0;
+}
---
+++ Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+
+            for (int j = 1; j <= i; j++)
+            {
+                cout << i;
+            }
+            cout << "\n";
+        
