a5e54b268215332ee2f52f8ef4e02bf6d0bccb89
sheemashariq1 <sheemashariq10@gmail.com>
2026-06-15 11:59:14 +0530

    Commit

---
+++ Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/Pattern_10.cpp
@@ -0,0 +1,38 @@
+/*
+ *
+ **
+ ***
+ ****
+ *****
+ ****
+ ***
+ **
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void pattern10(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
+        for (int i = n - 1; i > 0; i--)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+};
