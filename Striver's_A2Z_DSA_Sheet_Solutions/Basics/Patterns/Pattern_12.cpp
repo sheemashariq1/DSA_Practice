2a388af74f1e4a9ea8cd122bd0cdd035cd1ce670
sheemashariq1 <sheemashariq10@gmail.com>
2026-06-17 17:02:52 +0530

    Commit

---
+++ Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/Pattern_12.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    void pattern8(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+
+            for (int j = 1; j <= i; j++)
+            {
+                cout << j;
+            }
+
+            for (int j = 1; j <= 2 * (n - i); j++)
+            {
+                cout << " ";
+            }
+
+            for (int j = i; j >= 1; j--)
+            {
+                cout << j;
+            }
+
+            cout << endl;
+        }
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    Solution ob;
+    ob.pattern8(n);
+}
