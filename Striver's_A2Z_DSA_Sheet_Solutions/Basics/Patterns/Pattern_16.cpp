7ab86e3dd6655f4d4625e45a03114fa23a269afa
sheemashariq1 <sheemashariq10@gmail.com>
2026-06-22 23:16:44 +0530

    Commit

--- Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/Pattern_16.cpp
+++ Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/Pattern_16.cpp
@@ -6,3 +6,22 @@ DDDD
 EEEEE
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void pattern16(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            char ch = 'A' + i;
+            for (int j = 0; j <= i; j++)
+            {
+                cout << ch;
+            }
+            cout << endl;
+        }
+    }
+};
