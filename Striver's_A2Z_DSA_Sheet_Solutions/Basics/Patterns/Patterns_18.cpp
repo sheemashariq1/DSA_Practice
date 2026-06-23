be1d7eea5856b77a0d208f2d86173c69dcc679c9
sheemashariq1 <sheemashariq10@gmail.com>
2026-06-23 21:26:49 +0530

    Commit

--- Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/Patterns_18.cpp
+++ Striver's_A2Z_DSA_Sheet_Solutions/Basics/Patterns/Patterns_18.cpp
@@ -8,3 +8,28 @@ A B C D E
 
 #include <bits/stdc++.h>
 using namespace std;
+
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    void pattern18(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (char ch = ('A' + n - 1) - i; ch <= ('A' + n - 1); ch++)
+            {
+                cout << ch << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    Solution ob;
+    ob.pattern18(n);
+}
