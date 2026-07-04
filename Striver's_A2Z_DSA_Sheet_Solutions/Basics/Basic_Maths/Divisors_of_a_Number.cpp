e7a70c40c81451d99060a39f3cc6de044827383a
sheemashariq1 <sheemashariq10@gmail.com>
2026-07-05 01:07:05 +0530

    Commit all

--- Striver's_A2Z_DSA_Sheet_Solutions/Basics/Basic_Maths/Divisors_of_a_Number.cpp
+++ Striver's_A2Z_DSA_Sheet_Solutions/Basics/Basic_Maths/Divisors_of_a_Number.cpp
@@ -20,3 +20,25 @@ public:
         return result;
     }
 };
+
+// OR
+
+class Solution
+{
+public:
+    vector<int> divisors(int n)
+    {
+        vector<int> result;
+        for (int i = 1; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                result.push_back(i);
+                if (n / i != i)
+                    result.push_back(n / i);
+            }
+        }
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
